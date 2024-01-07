853d940bb3c9c1218116524efbac5819b42f37d6
edo1z <endo.yuta@logicky.com>
2024-01-07 15:29:22 +0900

    ログイン画面のUI作ったぜえ

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,3 +1,19 @@
-<div>
-    LOGIN
+<div class="p-5">
+    <h2 class="text-2xl">LOGIN</h2>
+
+    <div class="p-5 max-w-20">
+        <div class="mb-5">
+            <label class="label-text" for="email">Email</label>
+            <input class="input input-bordered" type="email" wire:model="email" id="email">
+            @error('email') <span>{{ $message }}</span> @enderror
+        </div>
+        <div class="mb-5">
+            <label class="label-text" for="password">Password</label>
+            <input class="input input-bordered" type="password" wire:model="password" id="password">
+            @error('password') <span>{{ $message }}</span> @enderror
+        </div>
+        <div>
+            <button class="btn btn-primary">LOGIN</button>
+        </div>
+    </div>
 </div>
