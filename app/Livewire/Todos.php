61574e74d4050ac2ab428ecc6a9130cbdc0fe72e
edo1z <endo.yuta@logicky.com>
2024-01-07 21:44:59 +0900

    TODOコンポーネントのUI作成

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class Todos extends Component
 {
+    public $todos = [];
+    public $title = '';
+
+    public function add()
+    {
+        $this->todos[] = $this->title;
+        $this->title = '';
+    }
+
     public function render()
     {
         return view('livewire.todos');
--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,4 +1,21 @@
-<div>
-    <h2>TODO</h2>
-    <button class="btn btn-primary">ADD</button>
+<div class="p-5">
+    <h2 class="text-2xl font-bold">TODO</h2>
+    <div class="mt-10 flex space-x-3">
+        <form wire:submit.prevent="add">
+            <input type="text" wire:model="title" class="input input-bordered">
+            <button class="btn btn-primary">ADD</button>
+        </form>
+    </div>
+    <div>
+        @foreach ($todos as $todo)
+        <div wire:key="{{ $todo }}" class="flex justify-between items-center border-b-2">
+            <div>{{ $todo }}</div>
+            <div class="flex space-x-2 py-2">
+                <button class="btn btn-primary">編集</button>
+                <button class="btn btn-primary">完了</button>
+                <button class="btn btn-primary">削除</button>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
